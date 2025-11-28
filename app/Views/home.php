72ee235229bc70bd83e6e1a015b03a591a42356f
Sown11 <sonb2004@gmail.com>
2025-11-28 18:22:26 +0700

    trang-chủ

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,117 +1,116 @@
 <div class="section">
 			<!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
+	<div class="container">
+		<!-- row -->
+		<div class="row">
 
+			<!-- section title -->
+			<div class="col-md-12">
+				<div class="section-title">
+					<h3 class="title">Sản Phẩm Mới</h3>
+					<div class="section-nav">
+						<ul class="section-tab-nav tab-nav">
+							<?php foreach ($danhmuc as $index => $dm): ?>
+								<li class="<?= $index === 0 ? 'active' : '' ?>">
+									<a data-toggle="tab" href="#tab<?= $dm['id'] ?>">
+										<?= htmlspecialchars($dm['name']) ?>
+									</a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
 
-					<!-- section title -->
-					<div class="col-md-12">
-						<div class="section-title">
-							<h3 class="title">New Products</h3>
-							<div class="section-nav">
-								<ul class="section-tab-nav tab-nav">
-									<li class="active"><a data-toggle="tab" href="#tab1">Iphone</a></li>
-									<li><a data-toggle="tab" href="#tab1">Ipad</a></li>
-								</ul>
+				</div>
+			</div>
+			<!-- /section title -->
+
+			<!-- Products tab & slick -->
+			<div class="row g">
+				<?php if (!empty($newProducts)): ?>
+					<?php foreach ($newProducts as $sp): ?>
+						<div class="col-md-3 col-sm-6 mb-4">
+							<div class="product">
+								<div class="product-img">
+									<img src="/Du-An/admin/image/<?= htmlspecialchars($sp['img'] ?? 'default.png') ?>" alt="<?= htmlspecialchars($sp['name']) ?>" class="img-fluid">
+									<div class="product-label">
+										<span class="new">NEW</span>
+									</div>
+								</div>
+
+								<div class="product-body">
+									<p class="product-category"><?= htmlspecialchars($sp['category_name']) ?></p>
+
+									<h3 class="product-name">
+										<a href="#"><?= htmlspecialchars($sp['name']) ?></a>
+									</h3>
+									<h4 class="product-price"><?= number_format($sp['price']) ?> ₫</h4>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+									<div class="product-btns">
+										<button class="add-to-wishlist"><i class="fa fa-heart-o"></i></button>
+										<button class="add-to-compare"><i class="fa fa-exchange"></i></button>
+										<button class="quick-view"><i class="fa fa-eye"></i></button>
+									</div>
+								</div>
+
+								<div class="add-to-cart">
+									<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+								</div>
 							</div>
 						</div>
-					</div>
-					<!-- /section title -->
-
-					<!-- Products tab & slick -->
-					<div class="col-md-3">
-						<div class="row">
-							<div class="products-tabs">
-								<!-- tab -->
-								<div id="tab1" class="tab-pane active">
-									<div class="products-slick" data-nav="#slick-nav-1">
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="/Public/img/product01.png" alt="">
-												<div class="product-label">
-													<span class="sale">-30%</span>
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
-												<div class="product-rating">
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-												</div>
-												<div class="product-btns">
-													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-													<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
-										</div>
-										<!-- /product -->
-
-										<!-- product -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-										
-					<!-- Products tab & slick -->
-				</div>
-				<!-- /row -->
+					<?php endforeach; ?>
+				<?php else: ?>
+					<p>Hiện chưa có sản phẩm nào.</p>
+				<?php endif; ?>
 			</div>
-			<!-- /container -->
-			 <div id="hot-deal" class="section">
-			<!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
-					<div class="col-md-12">
-						<div class="hot-deal">
-							<ul class="hot-deal-countdown">
-								<li>
-									<div>
-										<h3>02</h3>
-										<span>Days</span>
-									</div>
-								</li>
-								<li>
-									<div>
-										<h3>10</h3>
-										<span>Hours</span>
-									</div>
-								</li>
-								<li>
-									<div>
-										<h3>34</h3>
-										<span>Mins</span>
-									</div>
-								</li>
-								<li>
-									<div>
-										<h3>60</h3>
-										<span>Secs</span>
-									</div>
-								</li>
-							</ul>
-							<h2 class="text-uppercase">hot deal this week</h2>
-							<p>New Collection Up to 50% OFF</p>
-							<a class="primary-btn cta-btn" href="#">Shop now</a>
-						</div>
+	</div>
+</div>
+<div id="hot-deal" class="section">
+	<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="hot-deal">
+						<ul class="hot-deal-countdown">
+							<li>
+								<div>
+									<h3>02</h3>
+									<span>Days</span>
+								</div>
+							</li>
+							<li>
+								<div>
+									<h3>10</h3>
+									<span>Hours</span>
+								</div>
+							</li>
+							<li>
+								<div>
+									<h3>34</h3>
+									<span>Mins</span>
+								</div>
+							</li>
+							<li>
+								<div>
+									<h3>60</h3>
+									<span>Secs</span>
+								</div>
+							</li>
+						</ul>
+						<h2 class="text-uppercase">hot deal this week</h2>
+						<p>New Collection Up to 50% OFF</p>
+						<a class="primary-btn cta-btn" href="#">Shop now</a>
 					</div>
 				</div>
-				<!-- /row -->
 			</div>
-			<!-- /container -->
+			<!-- /row -->
 		</div>
-		</div>
+	<!-- /container -->
+	</div>
         
